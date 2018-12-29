df9efd01ede7c6218922becff67b284a199fd920
MohiuddinAkib <mohiuddinmostafakamal@gmail.com>
2018-12-29 10:48:33 +0600

    established connection between server and client socket

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express'),
   debug = require('debug')('app:heart'),
+  socket = require('socket.io'),
+  morgan = require('morgan'),
   app = express();
 
 // Serve static folders
 app.use(express.static(__dirname + '/public'));
 
+// Middlewares
+if (app.get('env') === 'development') {
+  app.use(morgan('dev'));
+  debug('Morgan enabled...');
+}
+
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => debug(`Server running on port ${PORT}`));
+
+// Set up socket
+const io = socket(server);
+
+io.on('connection', socket => {
+  debug('Made socket connection');
+});
